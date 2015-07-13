4bd96e0a1d96823fbba220c1a5ca0a7b89da7eca
Eric Schneider <franz.eric.schneider@gmail.com>
2015-07-13 10:34:11 -0400

    Updated test code for finger stress test

--- reflex_sf/src/reflex_sf/test_finger.py
+++ reflex_sf/src/reflex_sf/test_finger.py
@@ -7,19 +7,23 @@ def main():
     rospy.init_node("finger_test")
     pub = rospy.Publisher('/reflex_sf/command', SFPose, queue_size=2)
     cmd = SFPose()
+    rospy.sleep(1.0)
+    pub.publish(cmd); pub.publish(cmd)
+    pub.publish(cmd); pub.publish(cmd)
+    rospy.sleep(5.0)
+    cmd.f2 = 4.5
+    cmd.f3 = 4.0
     count = 0
 
     while not rospy.is_shutdown():
         rospy.loginfo("Opening hand")
-        cmd.f2 = 0.0
-        pub.publish(cmd)
-        pub.publish(cmd)
-        rospy.sleep(1.0)
+        cmd.f1 = 0.0
+        pub.publish(cmd); pub.publish(cmd)
+        rospy.sleep(2.0)
         rospy.loginfo("Closing hand, count %d", count)
-        cmd.f2 = 1.5
-        pub.publish(cmd)
-        pub.publish(cmd)
-        rospy.sleep(0.75)
+        cmd.f1 = 4.0
+        pub.publish(cmd); pub.publish(cmd)
+        rospy.sleep(2.0)
         count += 1
 
 if __name__ == '__main__':
